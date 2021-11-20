905380606207a61d34096e65effa8a588856459d
nabil <merchant.nabil@gmail.com>
2021-11-20 11:25:00 -0600

    saving the text entries to a local file.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # password-manager-gui
 This program creates a password manager using the TKinter library in Python. The password manager asks the user for some an email and a website. It also asks the user to enter a password or generate one on their behalf. This password is saved to a local text file.
 When the user asks for a randomply generated password, it is immediately saved to the clipboard to be pasted into a login field for a website.
+
+The cursor automatically goes to the first text box so user can start typing right away. 
+Email field is also prepopulated with a default email.
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    # Retrieve the typed strings from the entry boxes
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pw_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        # Empty the fields after saving them to a file
+        website_entry.delete(0, END)
+        pw_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 pic = PhotoImage(file="logo.png")
@@ -26,9 +40,11 @@ password_label.grid(column=0, row=3)
 # Entry boxes
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()  # Sets the cursor to this box as soon as the window opens
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "nabil@email.com")
 
 pw_entry = Entry(width=18)
 pw_entry.grid(column=1, row=3)
@@ -37,7 +53,7 @@ pw_entry.grid(column=1, row=3)
 generate_pwd_button = Button(text="Generate Password")
 generate_pwd_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=33)
+add_button = Button(text="Add", width=33, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
